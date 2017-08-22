9b3d883add26d071887eb45ae2df9f0d1bba4ad7
Corb3nik <ian@subnet192.com>
2017-08-21 20:10:09 -0400

    Adds error handling for incomplete commands

--- handlers/challenge_handler.py
+++ handlers/challenge_handler.py
@@ -3,26 +3,45 @@
 import shlex
 from helpers.command import *
 from helpers.invalid_command import *
+from unidecode import unidecode
 
 class AddCTFCommand(Command):
   """
     Add and keep track of a new CTF
   """
 
-  def __init__(self, *args):
+  def __init__(self, args):
+    if len(args) < 1:
+      raise InvalidCommand("Usage : add ctf <ctf_name>")
+
     self.channel = args[0]
 
   def execute(self, slack_client):
-    print(slack_client.api_call("channels.create",
+    response = (slack_client.api_call("channels.create",
         name=self.channel, validate=False))
+
+    if response['ok'] == False:
+      raise InvalidCommand("\"%s\" channel creation failed.\nError : %s" % (self.channel, response['error']))
 
 class AddChallengeCommand(Command):
   """
     Add and keep track of a new challenge for a given CTF
   """
 
-  def __init__(self, *args): pass
-  def execute(self, slack_client): pass
+  def __init__(self, args):
+    if len(args) < 2:
+      raise InvalidCommand("Usage : add challenge <challenge_name> <ctf_name>")
+
+    self.challenge = args[0]
+    self.ctf = args[1]
+    self.channel = "%s-%s" % (self.ctf, self.challenge)
+
+  def execute(self, slack_client):
+    response = slack_client.api_call("channels.create",
+        name=self.channel, validate=False)
+
+    if response['ok'] == False:
+      raise InvalidCommand("\"%s\" channel creation failed.\nError : %s" % (self.channel, response['error']))
 
 class ChallengeHandler:
   """
@@ -33,7 +52,7 @@ class ChallengeHandler:
     @ota_bot add ctf "defcon 25 quals"
 
     # Create a web-100 channel
-    @ota_bot add challenge "web 100" to "defcon 25 quals"
+    @ota_bot add challenge "web 100" "defcon 25 quals"
 
     # Kick member from other ctf challenge channels and invite the member to the web 100 channel
     @ota_bot working "web 100"
@@ -43,8 +62,8 @@ class ChallengeHandler:
    self.slack_client = slack_client
 
   def process(self, command, channel):
-    args = shlex.split(command.lower())
-
+    command_line = unidecode(command.lower())
+    args = shlex.split(command_line)
     command = None
 
     try:
@@ -60,9 +79,10 @@ class ChallengeHandler:
       elif args[:1] == ["working"]:
         command = WorkingCommand(args[1:])
 
+      if command:
+        command.execute(self.slack_client)
+
     except InvalidCommand as e:
       self.slack_client.api_call("chat.postMessage",
         channel=channel, text=e.message)
 
-    if command:
-      command.execute(self.slack_client)
