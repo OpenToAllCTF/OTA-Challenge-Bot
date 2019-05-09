d5524587c5bc0ad5acedb2eae27d370c8e02821e
kileak <razor99@gmx.de>
2019-05-09 18:47:35 +0200

    Add sysinfo admin command

--- handlers/bot_handler.py
+++ handlers/bot_handler.py
@@ -5,6 +5,7 @@ from handlers import handler_factory
 from handlers.base_handler import BaseHandler
 from util.githandler import GitHandler
 from util.loghandler import log
+import subprocess
 
 
 class PingCommand(Command):
@@ -45,7 +46,8 @@ class VersionCommand(Command):
             slack_wrapper.post_message(channel_id, message)
         except:
             log.exception("BotHandler::VersionCommand")
-            raise InvalidCommand("Sorry, couldn't retrieve the git information for the bot...")
+            raise InvalidCommand(
+                "Sorry, couldn't retrieve the git information for the bot...")
 
 
 class InviteCommand(Command):
@@ -60,7 +62,8 @@ class InviteCommand(Command):
         # strip uid formatting
         invited_users = [user.strip("<>@") for user in args]
         # remove already present members
-        invited_users = [user for user in invited_users if user not in current_members]
+        invited_users = [
+            user for user in invited_users if user not in current_members]
         failed_users = []
         for member in invited_users:
             if not slack_wrapper.invite_user(member, channel_id)["ok"]:
@@ -68,7 +71,24 @@ class InviteCommand(Command):
 
         if failed_users:
             log.exception("BotHandler::InviteCommand")
-            raise InvalidCommand("Sorry, couldn't invite the following members to the channel: " + ' '.join(failed_users))
+            raise InvalidCommand(
+                "Sorry, couldn't invite the following members to the channel: " + ' '.join(failed_users))
+
+
+class SysInfoCommand(Command):
+    """
+    Show information about system resources on the machine, otabot is running on.
+    """
+
+    @classmethod
+    def execute(cls, slack_wrapper, args, timestamp, channel_id, user_id, user_is_admin):
+        result = b"```\n"
+        result += b'\n'.join(subprocess.check_output(['top', '-bn1']).split(b"\n")[:15])
+        result += b"\n\n"
+        result += subprocess.check_output(['df', '-h'])
+        result += b"```\n"
+
+        slack_wrapper.post_message(user_id, result)
 
 
 class BotHandler(BaseHandler):
@@ -79,7 +99,8 @@ class BotHandler(BaseHandler):
             "ping": CommandDesc(PingCommand, "Ping the bot", None, None),
             "intro": CommandDesc(IntroCommand, "Show an introduction message for new members", None, None),
             "version": CommandDesc(VersionCommand, "Show git information about the running version of the bot", None, None),
-            "invite": CommandDesc(InviteCommand, "Invite a list of members (using @username) to the current channel (smarter than /invite)", ["user_list"], None)
+            "invite": CommandDesc(InviteCommand, "Invite a list of members (using @username) to the current channel (smarter than /invite)", ["user_list"], None),
+            "sysinfo": CommandDesc(SysInfoCommand, "Show system information", None, None, True)
         }
 
 
